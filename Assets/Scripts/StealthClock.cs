9caeccb775a177fdf183ad262cca8bb564554cb4
rbonneau <rbonneau@mail.csuchico.edu>
2016-02-11 14:46:15 -0800

    Branch stealthMiniGame initial commit.

---
+++ Assets/Scripts/StealthClock.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StealthClock : MonoBehaviour
+{
+
+
+	private int startDegree;
+	private int endDegree;
+
+	private int numberOfGuards;
+
+	//array of empty objects to hold line renderers, one object/line for each degree
+	private GameObject[] lines = new GameObject[360];
+
+	
+	private GameObject clockHand;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		//initialize lines for arc
+		lineSetup();
+
+		//get size of green area
+		startDegree = GetComponentInParent<StealthHand>().startDegree;
+		endDegree = GetComponentInParent<StealthHand>().endDegree;
+
+	}
+	
+
+	void lineSetup()
+	{
+
+		//create a line renderer for each object in the array
+		for(int i = 0; i < lines.Length; i++)
+		{
+
+			//set the position of each game object with line renderers
+			Vector3 tempPos = transform.position;
+			tempPos[1] = transform.position.y + 0.1f;
+			lines[i].transform.position = tempPos;
+
+			//add a line renderer to the empty game object
+			LineRenderer lRend = lines[i].AddComponent<LineRenderer>();
+
+			//only one line per game object
+			lRend.SetVertexCount(1);
+
+			//set all lines to red
+			lRend.SetColors(Color.red, Color.red);
+
+			//do math to point lines in correct directions, find end point for each line
+
+
+
+		}
+
+		
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+		//set
+		if(startDegree < endDegree)
+		{
+
+			for(int i = startDegree; i < endDegree; i++)
+			{
+
+				lines[i].GetComponent<LineRenderer>().SetColors(Color.green, Color.green);
+
+			}
+
+		}
+		else
+		{
+			for(int i = startDegree; i < lines.Length; i++)
+			{
+
+				lines[i].GetComponent<LineRenderer>().SetColors(Color.green, Color.green);
+
+			}
+
+			for(int i = 0; i < endDegree; i++)
+			{
+				lines[i].GetComponent<LineRenderer>().SetColors(Color.green, Color.green);
+			}
+		}
+		
+
+		//this won't work at runtime
+		drawArcs(GetComponentInParent<StealthHand>().startDegree, GetComponentInParent<StealthHand>().endDegree);
+
+	}
+
+	void drawArcs(int start, int end)
+	{
+
+		start = start % 360;
+		end = end % 360;
+
+		drawArc(start, end, Color.green);
+		drawArc(end, start, Color.red);
+
+	}
+
+	void drawArc(int start, int end, Color color)
+	{
+		
+		//draw color solid arc from start to end
+		//have to use line renderer
+
+	}
+}
---
+++ Assets/Scripts/StealthHand.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StealthHand : MonoBehaviour
+{
+
+	//green area on clock face between startDegree and endDegree
+	private float _startDegree;
+	private float _endDegree;
+
+
+	public int startDegree
+	{
+		get
+		{
+			return System.Convert.ToInt32(_startDegree);
+		}
+	}
+
+	public int endDegree
+	{
+		get
+		{
+			return System.Convert.ToInt32(_endDegree);
+		}
+	}
+
+
+	//number of successful presses before winning minigame
+	private int maxSuccess;
+	//current total of successful presses
+	private int currentSuccess;
+
+	//number of failed presses before losing minigame
+	private int maxFail;
+	//current total of failed presses
+	private int currentFail;
+
+	//number of enemies, affects clock speed
+	private int enemiesNearby;
+	//enemy distance to player, affects clock speed
+	private float distanceToPlayer;
+	//default speed of clock hand in degrees, 6f = 1rpm
+	private float speed = 6f;
+
+	//size of green area on clock, in degrees, affected by number of enemies and their distance to player
+	private float areaSize;
+
+	//StealthClock, the face of the clock, should be child of this StealthHand
+	GameObject clockFace;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		clockFace = transform.FindChild("StealthClock").gameObject;
+
+//START TEMP
+		//find/get number of enemies nearby
+		enemiesNearby = 1;
+
+		//set maxSuccess/maxFail
+		//should eventually take into account number of searching guards and their distance to player
+		maxSuccess = 3;
+		maxFail = 3;
+
+		//set rotation speed
+		speed *= enemiesNearby;
+//END TEMP
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//rotate hand
+		transform.RotateAround(clockFace.transform.position, clockFace.transform.up, Time.deltaTime * speed);
+		
+//		transform.RotateAround(transform.parent.position, transform.parent.transform.up, Time.deltaTime * speed);
+
+		if(Input.GetButtonDown("action"))
+		{
+
+			//check for success/failure
+			if((transform.rotation.y >= _startDegree) && (transform.rotation.y <= _endDegree))
+			{
+				currentSuccess++;
+			}
+			else
+			{
+				currentFail++;
+			}
+
+			if(currentSuccess >= maxSuccess)
+			{
+
+				//enemies return to normal
+
+
+			}
+			else if(currentFail >= maxFail)
+			{
+
+				//alert enemies to player position
+
+				
+			}
+
+		}
+
+		if(currentFail <= maxFail)
+		{
+
+			//do failure work
+
+		}
+		else if(currentSuccess >= maxSuccess)
+		{
+
+			//do successes work
+
+		}
+
+	}
+
+}
