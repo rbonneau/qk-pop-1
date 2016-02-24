069173993b71efa1da87bd9683584bc2f9442d38
rbonneau <rbonneau@mail.csuchico.edu>
2016-02-23 19:25:59 -0800

    Moved all success/fail logic to StealthClock. StealthHand only rotates itself and starts at a random angle now. Red/green zones reset on successes  up to maxSuccess. Reorganized StealthClock to be more modular.

--- Assets/Scripts/StealthClock.cs
+++ Assets/Scripts/StealthClock.cs
@@ -36,8 +36,34 @@ public class StealthClock : MonoBehaviour
 		}
 	}
 
-	float startWidth;
-	float endWidth;
+	//clock hand speed multiplyer, used by StealthHand object
+	public float _clockSpeed = 1f;
+
+	public float clockSpeed
+	{
+		get
+		{
+			return _clockSpeed;
+		}
+	}
+
+	//size of green area in degrees
+	private int _easySize = 45;
+	private int _mediumSize = 30;
+	private int _hardSize = 20;
+	private int _hellSize = 10;
+
+	//success/fail counts
+	private int _currentSuccess;
+	private int _currentFail;
+
+	//success/fail limits
+	public int maxSuccess;
+	public int maxFail;
+
+	//width of red/green lines start is center of circle, end is edge of circle
+	private float _startWidth;
+	private float _endWidth;
 
 	//number of guards near player
 	private int _numberOfGuards;
@@ -51,9 +77,9 @@ public class StealthClock : MonoBehaviour
 	}
 
 	//length of lines that make up red/green areas
-	private float lineLength;
+	private float _lineLength;
 	//degrees in a circle
-	private int degrees = 360;
+	private int _degrees = 360;
 
 	//starting point of each line, above center of clock face
 	public Vector3 startPos;
@@ -61,7 +87,7 @@ public class StealthClock : MonoBehaviour
 	//array of empty objects to hold line renderers, one object/line for each degree
 	public GameObject[] lines;
 	
-	private GameObject clockHand;
+	private GameObject _clockHand;
 
 	void Awake()
 	{
@@ -69,9 +95,11 @@ public class StealthClock : MonoBehaviour
 		//get the location of the player
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 
-		//get nearby enemies
+		//check for nearby enemies that are searching and not chasing player
+//AI
+
 //TEMP
-		_numberOfGuards = 1;
+//		_numberOfGuards = 1;
 //TEMP
 		
 		Collider[] enemyColliders = Physics.OverlapSphere(player.position, searchSize);
@@ -80,7 +108,7 @@ public class StealthClock : MonoBehaviour
 		{
 
 			//check for enemy tag
-			if(enemyColliders[i].CompareTag("enemy"))
+//			if(enemyColliders[i].CompareTag("enemy"))
 			{
 
 				//if enemy is searching for player
@@ -88,26 +116,25 @@ public class StealthClock : MonoBehaviour
 //AI				{
 
 					//add to list
-					enemyList.Add(enemyColliders[i].GetComponent<Enemy>());
+//					enemyList.Add(enemyColliders[i].GetComponent<Enemy>());
 
 //AI				}
 			}
 		}
 
-		_numberOfGuards = enemyList.Count;
+		//get the number of guards actively searching for player
+//		_numberOfGuards = enemyList.Count;
+		
+		//set difficulty
+		setDifficulty();
 
-		//calculate number of max successful presses
+		//set red and green zones
+//		setColors();
 
-		//calculate  number of max fail presses
 
-		//calculate size of green area
-
-		//set start and end of green area
-
-		//TESTING
-		_startDegree = 0;
-		_endDegree = 90;
-		//END TESTING
+//TESTING
+		Debug.Log("StealthClock Awake() complete");
+//END TESTING
 
 	}
 
@@ -116,30 +143,135 @@ public class StealthClock : MonoBehaviour
 	{
 
 		//initialize line parameters
-		lineLength = transform.localScale.x / 2f;
+		_lineLength = transform.localScale.x / 2f;
 		startPos = transform.position;
 		startPos = new Vector3(startPos.x, startPos.y + 0.1f, startPos.z);
-		startWidth = 0f;
-		endWidth = transform.localScale.x * Mathf.PI / 360f;
+		_startWidth = 0f;
+		_endWidth = transform.localScale.x * Mathf.PI / 360f;
 
 		//array of empty gameobjects to hold a single line renderer each
-		lines = new GameObject[degrees];
+		lines = new GameObject[_degrees];
 
 		//initialize lines for arc
 		lineSetup();
+		
+		//set red and green zones accordingly
+		setColors();
+
+		//get reference to clockHand
+		_clockHand = GetComponentInChildren<StealthHand>().gameObject;
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		//check for at least 1 suspicous enemy
+//AI
+		//check for no enemies chasing player
+//AI
+
+//should be if(Input.GetButtonDown("Action"))
+		if(Input.GetButtonDown("Jump"))
+		{
+
+			if(_startDegree < _endDegree)
+			{
+				//check for success/failure
+				if((startDegree <= _clockHand.transform.localEulerAngles.y) && (_clockHand.transform.localEulerAngles.y <= endDegree))
+				{
+					_currentSuccess++;
+
+//TESTING
+					print("SUCCESS: " + _clockHand.transform.localEulerAngles.y);
+//					print("currentSuccess: " + _currentSuccess);
+//					print("localEulerAngles: " + _clockHand.transform.localEulerAngles.y);
+					print("startDegree: " + startDegree);
+					print("endDegree: " + endDegree);
+//END TESTING
+					//check to see if zones need to be reset
+					if(_currentSuccess < maxSuccess)
+						{
+
+							setZones();
+
+						}
+
+				}
+				else
+				{
+					_currentFail++;
+
+//TESTING
+					print("FAIL: " + _clockHand.transform.localEulerAngles.y);
+//					print("currentFail: " + _currentFail);
+//					print("localEulerAngles: " + _clockHand.transform.localEulerAngles.y);
+					print("startDegree: " + startDegree);
+					print("endDegree: " + endDegree);
+//END  TESTING
+
+				}
+
+			}
+			//endDegree <= startDegree
+			else
+			{
+				if((_startDegree <= _clockHand.transform.localEulerAngles.y) || (_clockHand.transform.localEulerAngles.y <= endDegree))
+				{
+
+					_currentSuccess++;
+
+					//check to see if zones need to be reset
+					if(_currentSuccess < maxSuccess)
+					{
+
+						setZones();
+
+					}
+
+				}
+				else
+				{
+
+					_currentFail++;
+
+				}
+
+			}
+
+			//check for win condition
+			if(_currentSuccess >= maxSuccess)
+			{
+
+				//enemies return to normal
+				foreach(Enemy guard in enemyList)
+				{
+
+//AI				guard.enemyCurrentState = enemy.patrolState;
+
+				}
+
+			}
+			else if(_currentFail >= maxFail)
+			{
+
+				//alert enemies to player position
+				foreach(Enemy guard in enemyList)
+				{
+
+//AI				guard.enemyCurrentState = clockFace.chaseState;
+
+				}
+
+			}
+
+		}
 
 	}
 	
 	void lineSetup()
 	{
-
-		//set green and red with reduced opacity
-		Color colorG = new Color(0.0f, 1.0f, 0.0f, 0.9f);
-		Color colorR = new Color(1.0f, 0.0f, 0.0f, 0.9f);
-
-		//set color transparency
-//		colorG.a = 0.5f;
-//		colorR.a = 0.5f;
 
 		//create a line renderer for each object in the array
 		for(int i = 0; i < lines.Length; i++)
@@ -158,16 +290,152 @@ public class StealthClock : MonoBehaviour
 			//add a line renderer to the empty game object
 			LineRenderer lRend = lines[i].AddComponent<LineRenderer>();
 			//set line widths
-			lRend.SetWidth(startWidth, endWidth);
+			lRend.SetWidth(_startWidth, _endWidth);
 			//set line renderer material
 			lRend.material = new Material(Shader.Find("Particles/Additive"));
 
 			//only one line per game object
 			lRend.SetVertexCount(2);
+			
+			//do math to point lines in correct directions, find end point for each line
+			//initialize end point to current position of gameObject
+			linePos = startPos;
 
-			//set line colors accordingly
-			//check start and end boundaries
-			//green area fills upward from startDegree to endDegree
+			//find x and z position of end of line, y will remain the same
+			lineX = startPos.x + _lineLength * Mathf.Cos(Mathf.Deg2Rad * i);
+			linePos[0] = lineX;
+			lineZ = startPos.z + _lineLength * Mathf.Sin(Mathf.Deg2Rad * i);
+			linePos[2] = lineZ;
+
+			//set start and end points for the line
+			lRend.SetPosition(0, startPos);
+			lRend.SetPosition(1, linePos);
+			
+		}
+		
+	}
+
+	void setZones()
+	{
+		setDifficulty();
+		setColors();
+	}
+
+	//sets the values for zone areas, successes needed to win, fails allowed before losing, and speed of clock hand
+	//based on the number of guards presently searching for player
+	void setDifficulty()
+	{
+
+		//call AI manager for suspicious guards
+		_numberOfGuards = 1;
+
+		//choose random angle to start the green zone
+		_startDegree = Random.Range(0, 360);
+
+		//1 guard
+		if(_numberOfGuards < 2)
+		{
+
+			_endDegree = getEndDegree(_startDegree, _easySize);
+			maxSuccess = 3;
+			maxFail = 3;
+			_clockSpeed = 5;
+
+		}
+		//2-3 guards
+		else if(_numberOfGuards < 4)
+		{
+
+			_endDegree = getEndDegree(_startDegree, _mediumSize);
+			maxSuccess = 3;
+			maxFail = 2;
+			_clockSpeed = 10;
+
+		}
+		//4-9 guards
+		else if(_numberOfGuards < 10)
+		{
+
+			_endDegree = getEndDegree(_startDegree, _hardSize);
+			maxSuccess = 3;
+			maxFail = 1;
+			_clockSpeed = 20;
+
+		}
+		//10 or more guards
+		else
+		{
+
+			_endDegree = getEndDegree(_startDegree, _hellSize);
+			maxSuccess = 3;
+			maxFail = 1;
+			_clockSpeed = 40;
+
+		}
+
+	}
+
+	//called by setDifficulty()
+	//takes 2 integers: the start degree of the green area 0-359 and the size of the green area in degrees
+	int getEndDegree(int randomStart, int range)
+	{
+
+		int end;
+
+		//check for range
+		if(randomStart < 0)
+		{
+
+			randomStart = 0;
+
+		}
+
+		//find end degree
+		end = randomStart + range;
+
+		//check for upper bound
+		while(end > 360)
+		{
+
+			//move end within bounds
+			end = end - 360;
+
+			//check for 
+			if(randomStart == end)
+			{
+
+				//just set to easy range
+				_startDegree = 0;
+				end = _startDegree + _easySize;
+				
+				Debug.Log("StealthClock.getEndDegree: start == end");
+
+			}
+
+		}
+		
+		return end;
+
+	}
+
+	//sets the colors for each line renderer that make up the red/green zones
+	void setColors()
+	{
+
+		//set green and red with reduced opacity
+		Color colorG = new Color(0.0f, 1.0f, 0.0f, 0.9f);
+		Color colorR = new Color(1.0f, 0.0f, 0.0f, 0.9f);
+
+		LineRenderer lRend;
+
+		//set line colors accordingly
+		//check start and end boundaries
+		//green area fills upward from startDegree to endDegree
+		for(int i = 0; i < lines.Length; i++)
+		{
+
+			lRend = lines[i].GetComponent<LineRenderer>();
+
 			if(_startDegree < _endDegree)
 			{
 
@@ -179,7 +447,7 @@ public class StealthClock : MonoBehaviour
 				else
 				{
 
-					lRend.SetColors(Color.clear, colorR);
+					lRend.SetColors(colorR, colorR);
 
 				}
 
@@ -189,7 +457,7 @@ public class StealthClock : MonoBehaviour
 				if((_startDegree <= i) || (i <= _endDegree))
 				{
 
-					lRend.SetColors(Color.clear, colorG);
+					lRend.SetColors(colorR, colorG);
 
 				}
 				else
@@ -201,30 +469,8 @@ public class StealthClock : MonoBehaviour
 
 			}
 
-			//do math to point lines in correct directions, find end point for each line
-			//initialize end point to current position of gameObject
-			linePos = startPos;
-
-			//find x and z position of end of line, y will remain the same
-			lineX = startPos.x + lineLength * Mathf.Cos(Mathf.Deg2Rad * i);
-			linePos[0] = lineX;
-			lineZ = startPos.z + lineLength * Mathf.Sin(Mathf.Deg2Rad * i);
-			linePos[2] = lineZ;
-
-			//set start and end points for the line
-			lRend.SetPosition(0, startPos);
-			lRend.SetPosition(1, linePos);
-			
 		}
-		
-	}
-
-/*
-	// Update is called once per frame
-	void Update()
-	{
 
 	}
-*/
 
 }
--- Assets/Scripts/StealthHand.cs
+++ Assets/Scripts/StealthHand.cs
@@ -4,30 +4,9 @@ using System.Collections;
 public class StealthHand : MonoBehaviour
 {
 
-	//green area on clock face between startDegree and endDegree
-	public float startDegree;
-	public float endDegree;
-	
-	//number of successful presses before winning minigame
-	private int maxSuccess;
-	//current total of successful presses
-	private int currentSuccess;
-
-	//number of failed presses before losing minigame
-	private int maxFail;
-	//current total of failed presses
-	private int currentFail;
-
-	//number of enemies, affects clock speed
-	private int enemiesNearby;
-	//enemy distance to player, affects clock speed
-	private float distanceToPlayer;
 	//default speed of clock hand in degrees, 6f = 1rpm
-	private float speed = 6f;
-
-	//size of green area on clock, in degrees, affected by number of enemies and their distance to player
-//	private float areaSize;
-
+	private float _defaultSpeed = 6f;
+	
 	//StealthClock, the face of the clock, should be parent of this StealthHand
 	StealthClock clockFace;
 
@@ -35,35 +14,15 @@ public class StealthHand : MonoBehaviour
 	void Start()
 	{
 
-		//initialize success/fail counts
-		currentSuccess = 0;
-		currentFail = 0;
+//TESTING
+		Debug.Log("StealthHand Start() beginning");
+//END TESTING
 
 		//get the clock face
 		clockFace = transform.GetComponentInParent<StealthClock>();
-
-		//set green borders
-		startDegree = clockFace.startDegree;
-		endDegree = clockFace.endDegree;
-
-//START TEMP
-		//find/get number of enemies nearby
-//		enemiesNearby = 1;
-		enemiesNearby = clockFace.numberOfGuards;
-		if(enemiesNearby < 1)
-		{
-			enemiesNearby = 1;
-			Debug.Log("StealthHand: enemiesNearby < 1, set enemiesNearby to 1");
-		}
-
-		//set maxSuccess/maxFail
-		//should eventually take into account number of searching guards and their distance to player
-		maxSuccess = 3;
-		maxFail = 3;
-
-		//set rotation speed
-		speed *= enemiesNearby;
-//END TEMP
+		
+		//start the hand at a random degree
+		transform.RotateAround(transform.parent.position, transform.parent.transform.up, Random.Range(0, 360));
 
 	}
 
@@ -72,77 +31,7 @@ public class StealthHand : MonoBehaviour
 	{
 
 		//rotate hand
-		transform.RotateAround(transform.parent.position, transform.parent.transform.up, -Time.deltaTime * speed);
-
-		if(Input.GetButtonDown("Jump"))
-		{
-
-			if(startDegree < endDegree)
-			{
-				//check for success/failure
-				if((startDegree <= transform.localEulerAngles.y) && (transform.localEulerAngles.y <= endDegree))
-				{
-					currentSuccess++;
-
-					print("currentSuccess: " + currentSuccess);
-					print("localEulerAngles: " + transform.localEulerAngles.y);
-					print("startDegree: " + startDegree);
-					print("endDegree: " + endDegree);
-
-				}
-				else
-				{
-					currentFail++;
-
-					print("currentFail: " + currentFail);
-					print("localEulerAngles: " + transform.localEulerAngles.y);
-					print("startDegree: " + startDegree);
-					print("endDegree: " + endDegree);
-				}
-
-			}
-			else
-			{
-				if((startDegree <= transform.localEulerAngles.y) || (transform.localEulerAngles.y <= endDegree))
-				{
-
-					currentSuccess++;
-
-				}
-				else
-				{
-
-					currentFail++;
-
-				}
-
-			}
-			if(currentSuccess >= maxSuccess)
-			{
-
-				//enemies return to normal
-				foreach(Enemy guard in clockFace.enemyList)
-				{
-
-//AI					guard.enemyCurrentState = enemy.patrolState;
-
-				}
-
-			}
-			else if(currentFail >= maxFail)
-			{
-
-				//alert enemies to player position
-				foreach(Enemy guard in clockFace.enemyList)
-				{
-
-//AI					guard.enemyCurrentState = clockFace.chaseState;
-				
-				}
-				
-			}
-
-		}
+		transform.RotateAround(transform.parent.position, transform.parent.transform.up, -Time.deltaTime * _defaultSpeed * clockFace.clockSpeed);
 
 	}
 
